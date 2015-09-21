f7c1e1a8ed218faa97cf081ff6010cf0a213a4bf
Tanguy Coenen <tanguy.coenen@gmail.com>
2015-09-21 23:40:05 +0200

    stable line charts

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -3,40 +3,55 @@
  */
 
 d3.csv('file.csv', function(error,JSON_feature_data) {
-    console.log(JSON_feature_data);
     JSON_feature_data = JSON_create_date_ISO(JSON_feature_data,'date');
-    data = MG.convert.date(JSON_feature_data, 'date', '%Y-%m-%dT%H:%M:%SZ');
+    features_data = MG.convert.date(JSON_feature_data, 'date', '%Y-%m-%dT%H:%M:%SZ');
+    console.log(features_data);
     d3.csv('labels.csv', function(error,JSON_label_data) {
         if (error) return console.warn(error);
         labels_data = JSON_create_date_ISO(JSON_label_data,'date');
         labels_data = JSON_create_label_key(labels_data);
         labels_data = MG.convert.date(labels_data, 'date', '%Y-%m-%dT%H:%M:%SZ');
-        console.log(labels_data);
         MG.data_graphic({
             title: "rMSSD",
-            data: data,
-            width: 1200,
-            height: 400,
+            data: features_data,
+            width: 2200,
+            height: 300,
             right: 40,
             markers:labels_data,
-    //        linked:true,
+//            linked:true,
             area:false,
+            animate_on_load:true,
             target: '#graph1',
             x_accessor: 'date',
             y_accessor: ' rMSSD'
         });
         MG.data_graphic({
             title: "HF",
-            data: data,
-            width: 1200,
-            height: 400,
+            data: features_data,
+            width: 2200,
+            height: 300,
             right: 40,
             markers:labels_data,
-    //        linked:true,
+//            linked:true,
             area:false,
+            animate_on_load:true,
             target: '#graph2',
             x_accessor: 'date',
             y_accessor: ' HF'
+        });
+        MG.data_graphic({
+            title: "LFHF",
+            data: features_data,
+            width: 2200,
+            height: 300,
+            right: 40,
+            markers:labels_data,
+//            linked:true,
+            area:false,
+            animate_on_load:true,
+            target: '#graph3',
+            x_accessor: 'date',
+            y_accessor: ' LFHF'
         });
     });
 });
@@ -55,7 +70,6 @@ function JSON_create_date_ISO(JSON_object,key){
 Returns ISO 8601 formatted time
  */
 function date_splitter(old_format_date){
-    console.log(old_format_date);
     date = old_format_date.slice(0,10);
     time = old_format_date.slice(11,19);
     new_format_date = date+"T"+time+"Z";
